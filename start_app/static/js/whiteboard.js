9fb6c9ecdbd8bc9b9f89055b7bf9dcb240e68cc2
sammypotter <50476978+0spotter0@users.noreply.github.com>
2024-01-17 14:01:02 +0100

    added copy button for markdown code blocks —[Sammy's Macbook Air, Madrid, Spain, (c) U of R/Sammy Potter]

--- start_app/static/js/whiteboard.js
+++ start_app/static/js/whiteboard.js
@@ -57,6 +57,15 @@ function whiteboard_ready(){
     // }));
 }
 
+// Gets attribute data-raw-text, decodes it from Base64, then copies to clipboard
+function copyMarkdown(target) {
+    console.log("copying markdown");
+    var contentB64 = target.getAttribute('data-raw-text');
+    var decoded = atob(contentB64);
+    console.log(`copying content: ${decoded}`);
+    navigator.clipboard.writeText(decoded);
+}
+
 class WhiteboardContent {
     constructor(media) {
         this.media = media;
@@ -80,8 +89,16 @@ class WhiteboardContent {
                 break;
             case "markdown":
                 console.log("got markdown");
+                var contentStringEncoded = btoa(String.raw`${this.media.content}`);
                 this.mediaHTML = `
-                    <div class="markdownBlock">${marked.parse(this.media.content)}</div>
+                    <div class="markdownBlock relative">
+                        <div>
+                            ${marked.parse(this.media.content)}
+                        </div>
+                        <div class="copy-button absolute" style="top: 0; right: 0; margin: 1em;" data-raw-text="${contentStringEncoded}" onclick="copyMarkdown(this);">
+                            <svg class="svg-text-dark" xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><rect x="128" y="128" width="336" height="336" rx="57" ry="57" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/><path d="M383.5 128l.5-24a56.16 56.16 0 00-56-56H112a64.19 64.19 0 00-64 64v216a56.16 56.16 0 0056 56h24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>
+                        </div>
+                    </div>
                 `;
                 break;
             case "latex":
